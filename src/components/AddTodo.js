70757552c67e5a034745c13c9eb2ff130af1973c
carloscarvallo <carloscarvallo87@gmail.com>
2019-03-03 18:20:15 -0300

    basic todo

---
+++ src/components/AddTodo.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+
+export class AddTodo extends Component {
+  state = {
+    title: ""
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.addTodo(this.state.title);
+    this.setState({ title: "" });
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit} style={{ display: "flex" }}>
+        <input
+          style={{ flex: "10", padding: "5px" }}
+          type="text"
+          name="title"
+          value={this.state.title}
+          placeholder="Add Todo..."
+          onChange={this.onChange}
+        />
+        <input
+          type="submit"
+          value="Submit"
+          className="btn"
+          style={{ flex: "1" }}
+        />
+      </form>
+    );
+  }
+}
+
+AddTodo.propTypes = {
+  addTodo: PropTypes.func.isRequired
+}
+
+export default AddTodo;
